0193a463ae91705306e7e9ed49fbe968956cdee5
DiogoROliveira <diogo.rosas81@gmail.com>
2024-12-09 23:03:23 +0000

    removed csv and xml from tracking

--- rest_api_server/api/grpc/server_services_pb2_grpc.py
+++ rest_api_server/api/grpc/server_services_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import server_services_pb2 as server__services__pb2
+import api.grpc.server_services_pb2 as server__services__pb2
 
 GRPC_GENERATED_VERSION = '1.68.0'
 GRPC_VERSION = grpc.__version__
